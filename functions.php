25985c65a5e096992c5354eb3a3031f1729ca00d
IlarionHlukh <69106095+IlarionHlukh@users.noreply.github.com>
2020-10-01 23:34:17 +0300

    SQL||Homework

--- functions.php
+++ functions.php
@@ -4,24 +4,67 @@
     $time = time();
     $rand = rand(100, 500);
 
-    function makeRandomDateInclusive($startDate, $endDate)
-    {
-        return date("Y-m-d", strtotime("$startDate + " . rand(0, round((strtotime($endDate) - strtotime($startDate)) / (60 * 60 * 24))) . " days"));
-    }
-
     function InputDatatoDB()
-    {
-        $db = new mysqli("db", 'root', '', 'skillup');
+    {$db = new mysqli("db", 'root', '', 'skillup');
+        $users_sql_query = "INSERT INTO users (EMAIL, PASSWORD, CREATED_AT_DATE) VALUES ";
+        $users_array = array();
+        $domains_array = ['ukr.net', 'gmail.com', 'mail.ru', 'bigmir.net'];
+        for ($a = 1; $a < rand(500, 1000) + 1; $a++) {
+            array_push($users_array,
+                "('user{$a}@".$domains_array[rand(0, count($domains_array) - 1)]."', '".md5($a * 1000)."', '"
+                .date('Y-m-d', strtotime('2020-06-01') + (86400 * rand(0, (strtotime('2020-08-31') - strtotime('2020-06-01'))/86400)))."')");
+        }
+        $users_sql_query .= implode(', ', $users_array).';';
 
-        $timeDB = makeRandomDateInclusive('2020-06-01', '2020-08-31');
+        $db->query($users_sql_query);
 
-        $rand = rand(0, 100);
+        $products_sql_query = "INSERT INTO products (PRICE, NAME) VALUES ";
+        $products_array = array();
+        $companies = array('intel' => [700, 2500], 'amd' => [500, 2300], 'nvidia' => [150, 1000], 'zotac' => [10, 15],
+            'kingston' => [150, 720], 'asus' => [800, 3200], 'lenovo' => [1, 100], 'gigabyte' => [2, 200], 'hp' => [800, 2100]);
+        for ($a = 1; $a < rand(500, 1000) + 1; $a++) {
+            $companie = array_keys($companies)[rand(0, count($companies) - 1)];
+            array_push($products_array,
+                "(".rand($companies[$companie][0], $companies[$companie][1]).", '{$companie}{$a}')");
+        }
+        $products_sql_query .= implode(', ', $products_array).';';
 
-        $time = time();
-
-        $query = "INSERT INTO `test` SET `id`= '$rand',`email` = 'user+$time@gmail.com', `created_date` = '$timeDB'";
-        $db->query($query);
+        $db->query($products_sql_query);
+        header("Location:/");
     }
+function CreateRandomOrders()
+{
+    $db = new mysqli("db", 'root', '', 'skillup');
+    $users_db = $db->query("SELECT ID, CREATED_AT_DATE FROM users");
+    $users = $users_db->fetch_all(MYSQLI_ASSOC);
+    $selected_users_id_array_keys = array_rand($users, 100);
+    $products_db = $db->query("SELECT ID FROM products");
+    $products = $products_db->fetch_all(MYSQLI_ASSOC);
+    $orders_sql_query = "INSERT INTO orders (USER_ID, PRODUCT_ID, CREATED_AT_DATE) VALUES ";
+    $orders_array = array();
+    foreach ($selected_users_id_array_keys as $key => $value) {
+        $random_products = array_rand($products, rand(2, 8));
+        foreach ($random_products as $key => $product) {
+            $user_cr_date = date('d.m.Y', strtotime($users[$value]['CREATED_AT_DATE']));
+            $days_qty = date('d.m.Y') - $user_cr_date;
+            array_push($orders_array, "({$users[$value]['ID']}, {$products[$product]['ID']}, 
+                '".date('Y-m-d', strtotime($user_cr_date) + (86400 * rand(0, $days_qty)))."')");
+        }
+    }
+    $orders_sql_query .= implode(', ', $orders_array).';';
 
+    $db->query($orders_sql_query);
+    header("Location:/");
+}
 
+function deleteDBdata() {
+
+    $db = new mysqli("db", 'root', '', 'skillup');
+
+    $query="SET foreign_key_checks = 0; call clear3();  begin truncate table users; truncate table products; truncate table orders; end; SET foreign_key_checks = 1";
+
+    $db->multi_query($query);
+
+    header("Location:/");
+}
 
--- index.php
+++ index.php
@@ -8,11 +8,11 @@ switch ($action) {
     case "InputDate":
         InputDatatoDB();
         break;
-    case "ToRandUsers":
-        adminAddProductEndpoint();
+    case "MakeRandomOrders":
+        CreateRandomOrders();
         break;
     case "DeleteData":
-        adminChangeRoleEndpoint();
+        deleteDBdata();
         break;
 }
 
@@ -39,7 +39,7 @@ $html= '
 </head>
 <body><link rel="stylesheet"  media="screen" type="text/css" />
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-    <h3 class="my-0 mr-md-auto font-weight-normal" href="/"><strong>PHP</strong></h3>
+    <h3 class="my-0 mr-md-auto font-weight-normal" href="/"><strong>PHP & SQL</strong></h3>
 </div>
 <div
 <html>
@@ -50,8 +50,131 @@ $html= '
     </head>
 <body>
 <a class="btn btn-primary" href="/?action=InputDate">Внесення тестових даних</a>
-<a class="btn btn-warning">Вибір рандомних користувачів</a>
-<a class="btn btn-danger">Видалення даних БД</a>
-<div>
-   ';
- echo $html;
+<a class="btn btn-warning" href="/?action=MakeRandomOrders">Створення замовлень</a>
+<a class="btn btn-danger" href="/?action=DeleteData">Видалення даних БД</a>';
+echo $html;
+$db = new mysqli("db", 'root', '', 'skillup');
+$html='<table mt-10 width="460" height="49" border="0" cellpadding="0" cellspacing="2">';
+$html .='<tr>';
+$html .='<td height="19" valign="top">
+<h7><strong>Користувачі+замовлення в порядку спадання</strong></h7>';
+$res = $db->query("SELECT u.email, USER_ID, COUNT(DISTINCT o.ID) FROM orders o join users u on u.id = o.user_id GROUP BY o.user_id ORDER BY 3 DESC;");
+$users = $res->fetch_all(MYSQLI_ASSOC);
+$html.='<div class="container" align="center">';
+$html .= '<table border="2" cellpadding="5" bordercolor="blue">';
+$html .= '<thead>';
+$html .= '<th>Користувач</th>';
+$html .= '<th>ID користувача</th>';
+$html .= '<th>Кількість замовлень</th>';
+$html .= '</thead>';
+$html .= '<tbody>';
+foreach ($users as $user) {
+    $html .= "<tr><td align='center'>" . $user['email'] . "</td><td align='center'>" . $user['USER_ID'] . "</td><td align='center'>" . $user['COUNT(DISTINCT o.ID)'] . "</td></tr>";
+}
+$html .= '</tbody>';
+echo $html;
+
+$res_u = $db->query("select u.email,sum(p.price) as total_price from orders o join users u on u.id = o.user_id join products p on p.id = o.product_id group by u.email order by total_price desc;");
+
+$users_total = $res_u->fetch_all(MYSQLI_ASSOC);
+$html.='<div class="container" align="center">';
+$html = '<table border="2" cellpadding="5" bordercolor="orange">';
+$html .= '</tbody>';
+$html .= '<tbody>';
+echo $html;
+$html .= '</td>';
+$html .='</tr>';
+$html .='</table></td>';
+$html .='<td valign="top"><table>';
+$html .='<tr>';
+$html .='<h7><strong>Загальна сумма витрачених коштів</strong></h7>';
+$html .='<td height="48" valign="top">';
+$html .= '<table width="230" height="50" border="2" cellpadding="5" bordercolor="orange">';
+$html .='<thead>';
+$html .= '<th>Користувач</th>';
+$html .= '<th>Загальна сума $</th>';
+$html .= '</thead>';
+$html .= '<tbody>';
+foreach ($users_total as $user) {
+    $html .= "<tr><td align='center'>" . $user['email'] . "</td><td align='center'>" . $user['total_price'] . "</td></tr>";
+}
+$html .= '</tbody>';
+$html .='</td>';
+$html .='</tr>';
+$html .='</table></td>';
+$html .='</tr>';
+$html .='</table>';
+echo $html;
+
+$res_top = $db->query("select * from (select o.product_id, count(distinct o.id) from orders o group by o.product_id order by 2 desc) a join products p on p.id = a.product_id limit 3;");
+
+$users_top = $res_top->fetch_all(MYSQLI_ASSOC);
+
+$html = '<table>';
+$html .= '</tbody>';
+$html .= '<tbody>';
+echo $html;
+$html .= '</td>';
+$html .='</tr>';
+$html .='</table></td>';
+$html .='<td valign="top"><table>';
+$html .='<tr>';
+$html .='<h7><strong>ТОП 3 найпопулярніші товари</strong></h7>';
+$html .='<td height="48" valign="top">';
+$html.='<div class="container" align="center">';
+$html .= '<table border="2" cellpadding="5" bordercolor="red">';
+$html .='<thead>';
+$html .= '<th>Назва товару</th>';
+$html .= '<th>Ціна $</th>';
+$html .= '<th>Кількість заказів</th>';
+$html .= '</thead>';
+$html .= '<tbody>';
+foreach ($users_top as $user) {
+    $html .= "<tr><td align='center'>" . $user['name'] . "</td><td align='center'>" . $user['price'] . "</td><td align='center'>" . $user['count(distinct o.id)'] . "</td></tr>";
+}
+$html .= '</tbody>';
+$html .='</td>';
+$html .='</tr>';
+$html .='</table></td>';
+$html .='</tr>';
+$html .='</table>';
+echo $html;
+$res_date = $db->query("select distinct u.email, o.id, o.created_at_date from orders o join users u on u.id = o.user_id where o.created_at_date between '2020-07-01' and '2020-07-30' order by created_at_date asc;");
+
+$users_date = $res_date->fetch_all(MYSQLI_ASSOC);
+
+$html = '<table>';
+$html .= '</tbody>';
+$html .= '<tbody>';
+echo $html;
+$html .= '</td>';
+$html .='</tr>';
+$html .='</table></td>';
+$html .='<td valign="top"><table>';
+$html .='<tr>';
+$html .='<h7><strong>Замовлення користувачів з 01.07.2020 по 30.07.2020</strong></h7>';
+$html .='<td height="48" valign="top">';
+$html.='<div class="container" align="center">';
+$html .= '<table width="460" height="49" border="2" cellpadding="5" bordercolor="green">';
+$html .='<thead>';
+$html .= '<th>Користувач</th>';
+$html .= '<th>ID замовлення</th>';
+$html .= '<th>Дата</th>';
+$html .= '</thead>';
+$html .= '<tbody>';
+foreach ($users_date as $user) {
+    $html .= "<tr><td align='center'>" . $user['email'] . "</td><td align='center'>" . $user['id'] . "</td><td>" . $user['created_at_date'] . "</td></tr>";
+}
+$html .= '</tbody>';
+$html .='</td>';
+$html .='</tr>';
+$html .='</table></td>';
+$html .='</tr>';
+$html .='</table>';
+echo $html;
+
+
+
+
+
+
